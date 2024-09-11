1e39f8579dca23a3dec7e4ceaee7e973950799c2
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-09-11 11:02:02 +0500

    created a funtion that appends new node at the end of the list

--- linked-list.js
+++ linked-list.js
@@ -1,11 +1,48 @@
 class LinkedList {
-  
+	constructor() {
+		this.head = null;
+	}
+	append(value) {
+		let newNode = new Node(value);
+		if (!this.head) {
+			this.head = newNode;
+		} else {
+			let current = this.head;
+			if (current.next) {
+				current = current.next;
+			}
+			current.next = newNode;
+		}
+	}
 }
 
 class Node {
-	constructor() {
-		this.value = null;
+	constructor(value = null, next = null) {
+		this.value = value;
 		this.next = null;
 	}
 }
 
+const list = new LinkedList();
+/* console.log(list); */
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+/* function findperson(list, name) {
+	if (!list.next) {
+		return -1;
+	}
+	if (list.value == name) {
+		return name;
+	}
+
+	return findperson(list.next, name);
+}
+
+console.log(findperson(list.head, "cat"));
+ */
