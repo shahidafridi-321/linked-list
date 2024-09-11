05f4073b184baeafd7166409349122763aba408e
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-09-11 21:19:18 +0500

    added approciate comments

--- linked-list.js
+++ linked-list.js
@@ -1,34 +1,41 @@
+// linked list manager class resposible for different operation on the list
 class LinkedList {
 	constructor() {
+		// initializd head as null
 		this.head = null;
 	}
+
+	// takes a value and appends a new node with that value it at the end of the list
 	append(value) {
 		let newNode = new Node(value);
 		if (!this.head) {
-			this.head = newNode;
+			this.head = newNode; // if the list is empty,add node at start
 		} else {
 			let current = this.head;
 			while (current.next) {
-				current = current.next;
+				current = current.next; // move to last node
 			}
-			current.next = newNode;
+			current.next = newNode; // append at the end
 		}
 	}
 
+	// appends value at the start of the list
 	prepend(value) {
 		let newNode = new Node(value);
 		if (!this.head) {
-			this.head = newNode;
+			this.head = newNode; //if the list is empty,add node at start
 		} else {
-			let nextNode = this.head;
+			let currentList = this.head;
 			this.head = newNode;
-			newNode.next = nextNode;
+			newNode.next = currentList; // append the node at the start of the currentList
 		}
 	}
 
+	// returns the size of the list
 	size() {
 		let size = 0;
 		if (!this.head) {
+			// checks if list is empty
 			return size;
 		} else {
 			let current = this.head;
@@ -40,23 +47,28 @@ class LinkedList {
 		return size;
 	}
 
+	// returns the first node of the list
 	headNode() {
 		return this.head;
 	}
 
+	// returns the last node of the list
 	tail() {
 		if (!this.head) {
-			return this.head;
+			return this.head; // if the list is empty return the head which will be the last node as well
 		} else {
 			let current = this.head;
 			while (current.next) {
+				// traverse the list until the last node
 				current = current.next;
 			}
-			return current;
+			return current; // return last node
 		}
 	}
 
+	// returns value at the given index
 	at(index) {
+		// if the index is negetive returns a message
 		if (index < 0) {
 			return `index out of range`;
 		}
@@ -65,38 +77,45 @@ class LinkedList {
 		let current = this.head;
 		while (current) {
 			if (counter === index) {
-				return current;
+				// traverse the list until found the node
+				return current; // if found return the node
 			}
 			current = current.next;
 			counter++;
 		}
-		return "index out of range";
+		return "index out of range"; // if not found returns a message
 	}
 
+	// removes a node at the end of the list
 	pop() {
+		// if the list is empty returns empty list
 		if (!this.head) {
 			return this.head;
 		}
-
+		// the list has only a single node
 		let current = this.head;
 		if (!current.next) {
 			let poppedValue = current.value;
-			this.head = null;
-			return poppedValue;
+			this.head = null; // make the list empty
+			return poppedValue; // returns the value removed
 		}
 
 		while (current.next) {
+			// travese the list
 			if (!current.next.next) {
-				let poppedValue = current.next.value;
-				current.next = null;
-				return poppedValue;
+				// checks if the last node is null
+				let poppedValue = current.next.value; // store the value at the second last node
+				current.next = null; // remove the second last node
+				return poppedValue; // return the removed value
 			}
 			current = current.next;
 		}
 	}
 
+	// returns true if the given value is found in list otherwise returns false
 	contains(value) {
 		if (!this.head) {
+			// Return false if the list is empty
 			return false;
 		}
 		let current = this.head;
@@ -114,9 +133,10 @@ class LinkedList {
 		}
 	}
 
+	// returns index of the given value if found otherwise returns null
 	find(value) {
 		if (!this.head) {
-			return null;
+			return null; // Return null if the list is empty
 		}
 		let current = this.head;
 		let index = 0;
@@ -128,6 +148,22 @@ class LinkedList {
 			current = current.next;
 		}
 		return null;
+	}
+	// formates the lsit
+	toString() {
+		if (!this.head) {
+			// Return null if the list is empty
+			return null;
+		}
+		let current = this.head;
+		let string = "";
+		while (current) {
+			// traverse the list
+			string += `( ${current.value} ) -> `; // add the current value to the string
+			current = current.next;
+		}
+		string += "null";
+		return string; // returns the final template string
 	}
 }
 
@@ -150,6 +186,6 @@ list.prepend("hourse");
 
 console.log(list.find("turtle"));
 
-let value = list.pop();
+/* let value = list.pop(); */
 
-console.log(list.find("turtle"));
+console.log(list.toString());
